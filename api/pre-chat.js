3d4c3d010a0ae66c26d84262544c864f8c8ebb60
Swapnesh <spawar@seydeltech.com>
2026-06-01 10:44:45 +0530

    Update pre-chat.js

--- api/pre-chat.js
+++ api/pre-chat.js
@@ -6,7 +6,10 @@ module.exports = async function handler(req, res) {
 
   const ALLOWED_ORIGINS = [
     'https://detaelectrical.com.au',
-    'https://e24965-c4.myshopify.com',
+    'https://e24965-c4.myshopify.com', // Deta
+    'https://www.arlec.com.au/',
+    'arlec-au.myshopify.com'
+    
   ];
 
   const requestOrigin = req.headers.origin || '';
